60ea3c209adc6d770785dfdda176999ecc17261c
Yeeun <kye3314@gmail.com>
2023-04-03 20:04:46 +0900

    녹음 수집기 배치파일 완성 (지원)

--- radio.bat
+++ radio.bat
@@ -1,12 +1,21 @@
 cd .\
 
+set RADIO_NAME=%0
+
+if not exist %cd%\VisualRadio\radio_storage\%RADIO_NAME%\ (
+    mkdir %cd%\VisualRadio\radio_storage\%RADIO_NAME%
+)
+
+set RADIO_DATE=%date:~2,2%%date:~5,2%%date:~8,2%
+if not exist %cd%\VisualRadio\radio_storage\%RADIO_NAME%\%RADIO_DATE%\ (
+    mkdir %cd%\VisualRadio\radio_storage\%RADIO_NAME%\%RADIO_DATE%
+)
+
 set PROGRAM_NAME=raw
-
-set REC_DATE=%date:~0,4%%date:~5,2%%date:~8,2%
  
-set MP3_FILE_NAME=%PROGRAM_NAME%.wav
+set MP3_FILE_NAME=VisualRadio\radio_storage\%RADIO_NAME%\%RADIO_DATE%\%PROGRAM_NAME%.wav
 
-set RECORD_MINS=3600
+set RECORD_MINS=10
 
 @ echo off
 python radio_mbcfm4u.py > output
