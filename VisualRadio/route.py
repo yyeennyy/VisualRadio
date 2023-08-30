c6756cda8bf2f96a402b0cad950d3aa5b80dfb3e
yyeennyy <kye3314@gmail.com>
2023-08-30 15:52:01 +0900

    del_all 정상동작함(commit 누락 문제)

--- VisualRadio/route.py
+++ VisualRadio/route.py
@@ -139,7 +139,9 @@ def process_audio_file(broadcast, name, date):
     with app.app_context():
         process = bring_process(broadcast, name, date)
         process.set_raw()
-        # (필요시 사용) 모든 Process 진행사항 지우기 : process.del_all()
+        # (필요시 사용) 모든 Process 진행사항 지우기 
+        # process.del_all()
+        # commit(process)
         try:
             # start!
             s_time = time.time()
